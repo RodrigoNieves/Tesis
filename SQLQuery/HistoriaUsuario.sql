1cc3b63892d992c80a4bfc1de0a4ecbed42912be
RodrigoNieves <nieves_nieves_nieves@hotmail.com>
2015-02-25 13:19:55 -0600

    Agrega Query que obtine historia de problemas enviados

---
+++ SQLQuery/HistoriaUsuario.sql
@@ -0,0 +1,25 @@
+/* 
+ * Obtiene el historial de cada uno de los elementos
+ * Preguntar a Felix por que algunos no tienen hora?
+ */
+SELECT usuario,
+       DateTime,
+	   problema FROM (SELECT
+		usuario,
+		problema,
+		puntos,
+		hora/100000000 as year,
+		(hora/1000000) % 100 as month,
+		(hora/10000)   % 100 as day,
+		(hora/100)     % 100 as hour,
+		hora           % 100 as minutes,
+		DATETIMEFROMPARTS(
+			hora/100000000,
+			(hora/1000000) % 100,
+			(hora/10000)   % 100,
+			(hora/100)     % 100,
+			hora           % 100,
+			0,0) as DateTime
+		FROM Karelotitlan.dbo.UsuarioProblema
+	WHERE puntos = 100 and hora != 0) as submitions
+	order by usuario, DateTime
