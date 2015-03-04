8ab6df21ae8caf805f8447d54e2d324afb6c8591
RodrigoNieves <nieves_nieves_nieves@hotmail.com>
2015-03-04 16:03:58 -0600

    Agregando caluclo de las probabilidades de resolver un problema

--- Simulacion/Simulacion/Simulador.cs
+++ Simulacion/Simulacion/Simulador.cs
@@ -14,8 +14,10 @@ namespace Simulacion
         Dictionary<int, Dictionary<int, List<int>>> nivelUsurios;        // [Tema][IdUsuario]
         Dictionary<int, Problema> problemas;
         Dictionary<int, Dictionary<int, int>> conteo;                   // [Problema][Nivel]
+        Dictionary<int, Dictionary<int, double>> pResolver;             // [Problema][Nivel]
         bool incluyeCero = true;
         List<Tema> temas;
+        List<Dificultad> dificultades;
         int rango = 3;  // tamanio de ventana de analisis para determinar el nivel de usuario
         int min_sup = 2; // minimo numero de problemas para considerar que esta en ese nivel
         private int calculaNivel(Queue<int> ventana)
@@ -44,6 +46,7 @@ namespace Simulacion
                 problemas[problem.idProblema] = problem;
             }
             temas = karelotitlan.temas();
+            dificultades = karelotitlan.dificultades();
             historiasDificultad = new Dictionary<int, Dictionary<int, List<int>>>();
             nivelUsurios = new Dictionary<int, Dictionary<int, List<int>>>();
             foreach (var tema in temas)
@@ -105,11 +108,12 @@ namespace Simulacion
                         if (tema.idTema == problemas[problema].idTema)
                         {
                             int idUsuario = usuario.Key;
-                            if (conteo[problema] == null)
+
+                            if (!conteo.ContainsKey(problema))
                             {
                                 conteo[problema] = new Dictionary<int, int>();
                             }
-                            if (conteo[problema][nivelAct] == null)
+                            if (!conteo[problema].ContainsKey(nivelAct))
                             {
                                 conteo[problema][nivelAct] = 0;
                             }
@@ -118,6 +122,31 @@ namespace Simulacion
                     }
                 }
             }
+            pResolver = new Dictionary<int, Dictionary<int, double>>();
+            foreach (var problema in problemas)
+            {
+                int total = 0;
+                int maximo = 1;
+                foreach (var nivel in dificultades)
+                {
+                    if (!conteo[problema.Key].ContainsKey(nivel.idDificultad))
+                    {
+                        conteo[problema.Key][nivel.idDificultad] = 0;
+                    }
+                    total += conteo[problema.Key][nivel.idDificultad];
+                    conteo[problema.Key][nivel.idDificultad] = total;
+                    if (total > maximo)
+                    {
+                        maximo = total;
+                    }
+                }
+                pResolver[problema.Key] = new Dictionary<int, double>();
+                foreach (var nivel in dificultades)
+                {
+                    pResolver[problema.Key][nivel.idDificultad] = conteo[problema.Key][nivel.idDificultad] / (double)maximo;
+                }
+            }
+            
         }
         public string testIniciaModelo()
         {
@@ -146,6 +175,17 @@ namespace Simulacion
                     }
                     result.Append("\r\n");
                 }
+            }
+            result.Append("\r\n");
+            foreach (var problema in problemas)
+            {
+                result.Append(problema.Key.ToString() + ",");
+                foreach (var tema in pResolver[problema.Key])
+                {
+                    result.Append(tema.Key.ToString() + "," + tema.Value.ToString() + ",");
+                }
+
+                result.Append("\r\n");    
             }
             return result.ToString();
         }
