79a5b3c97daf196253ce10c9e0c74ac28c7d1c49
RodrigoNieves <nieves_nieves_nieves@hotmail.com>
2015-03-12 12:46:08 -0600

    Agregando Connection String a Base de Datos Simulacion

--- Simulacion/Simulacion/Properties/Settings.Designer.cs
+++ Simulacion/Simulacion/Properties/Settings.Designer.cs
@@ -32,5 +32,16 @@ namespace Simulacion.Properties {
                 return ((string)(this["KarelotitlanConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=CICPC;Initial Catalog=SimulacionKarelotitlan;Integrated Security=True" +
+            "")]
+        public string SimulacionConnectionString {
+            get {
+                return ((string)(this["SimulacionConnectionString"]));
+            }
+        }
     }
 }
