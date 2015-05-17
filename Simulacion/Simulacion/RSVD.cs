62046cc861c11acc4e4909cb02d11fec5c7e0d71
Rodrigo <rodrigonieves@live.com.mx>
2015-05-16 19:55:15 -0500

    Agregando esqueleto de ultimo recomendador

---
+++ Simulacion/Simulacion/RSVD.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulacion
+{
+    class RSVD: Recomendador
+    {
+        void Recomendador.iniciaRecomendador()
+        {
+            throw new NotImplementedException();
+        }
+
+        void Recomendador.realizaAnalisis()
+        {
+            throw new NotImplementedException();
+        }
+
+        int Recomendador.recomendacion(int idCompetidor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
